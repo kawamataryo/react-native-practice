129a0a69a6d587302709ce24033eb12ec312ade9
ryo <ba068082@gmail.com>
2019-08-13 22:43:56 +0900

    ESLint対応

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import {Root} from 'native-base';
+import React from "react";
+import { Root } from "native-base";
 import MyCamera from "./src/components/indexScreen/MyCamera";
 
 export default function App() {
   return (
-      <Root>
-        <MyCamera/>
-      </Root>
+    <Root>
+      <MyCamera />
+    </Root>
   );
 }
-
