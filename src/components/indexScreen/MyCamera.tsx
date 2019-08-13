e1c24ca616d2ef25af5a2a3dd22c3e61df4b3574
ryo <ba068082@gmail.com>
2019-08-13 22:20:09 +0900

    ESLintの設定追加

--- src/components/indexScreen/MyCamera.tsx
+++ src/components/indexScreen/MyCamera.tsx
@@ -1,15 +1,15 @@
-import React, {createRef} from 'react';
-import {View} from 'react-native';
-import {Button, Container, Icon, Text} from 'native-base';
-import {Camera} from 'expo-camera';
-import * as Permissions from 'expo-permissions';
+import React, { createRef } from "react";
+import { View } from "react-native";
+import { Button, Container, Icon, Text } from "native-base";
+import { Camera } from "expo-camera";
+import * as Permissions from "expo-permissions";
 import Environment from "../../../config/enviroment";
 import MyModal from "./MyModal";
 
-type Props = {}
+interface Props {}
 
-type State = {
-  hasCameraPermission: boolean
+interface State {
+  hasCameraPermission: boolean;
   ocrText: string;
   isModalVisible: boolean;
   loading: boolean;
@@ -18,20 +18,20 @@ type State = {
 export default class MyCamera extends React.Component<Props, State> {
   state = {
     hasCameraPermission: null,
-    ocrText: '',
+    ocrText: "",
     isModalVisible: false,
-    loading: false,
+    loading: false
   };
 
   async componentDidMount() {
-    const {status} = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({hasCameraPermission: status === 'granted'});
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === "granted" });
   }
 
   private cameraRef = createRef<Camera>();
 
   toggleModal = () => {
-    this.setState({isModalVisible: !this.state.isModalVisible, ocrText: ''})
+    this.setState({ isModalVisible: !this.state.isModalVisible, ocrText: "" });
   };
 
   takePicture = async () => {
@@ -39,79 +39,91 @@ export default class MyCamera extends React.Component<Props, State> {
       // modalの表示
       this.toggleModal();
       // 撮影
-      let photo = await this.cameraRef.current.takePictureAsync({base64: true});
+      let photo = await this.cameraRef.current.takePictureAsync({
+        base64: true
+      });
       // cloudVisionへデータを送信
       this.sendCloudVision(photo.base64);
     }
   };
 
   sendCloudVision = async (image: string) => {
-    this.setState({loading: true, isModalVisible: true})
+    this.setState({ loading: true, isModalVisible: true });
     let body = this.createRequestBody(image);
     let response = await fetch(
-        "https://vision.googleapis.com/v1/images:annotate?key=" +
+      "https://vision.googleapis.com/v1/images:annotate?key=" +
         Environment["GOOGLE_CLOUD_VISION_API_KEY"],
-        {
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-          },
-          method: "POST",
-          body: body
-        }
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: body
+      }
     );
     const resJson = await response.json();
     this.setState({
       ocrText: resJson.responses[0].textAnnotations[0].description as string,
       loading: false
-    })
+    });
   };
 
   createRequestBody = (image: string): string => {
     return JSON.stringify({
       requests: [
         {
-          features: [
-            {type: "TEXT_DETECTION", maxResults: 1},
-          ],
+          features: [{ type: "TEXT_DETECTION", maxResults: 1 }],
           image: {
             content: image
           }
         }
       ]
-    })
+    });
   };
 
   render() {
-    const {hasCameraPermission} = this.state;
+    const { hasCameraPermission } = this.state;
     if (hasCameraPermission === null) {
-      return <View/>;
+      return <View />;
     } else if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>;
     } else {
       return (
-          <Container style={{flex: 1}}>
-            <Camera style={{flex: 1}}
-                    ref={this.cameraRef}
-            >
-              <View style={{flex: 1}}>
-                <Button rounded icon onPress={this.takePicture} style={{
-                  position: 'absolute',
+        <Container style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} ref={this.cameraRef}>
+            <View style={{ flex: 1 }}>
+              <Button
+                rounded
+                icon
+                onPress={this.takePicture}
+                style={{
+                  position: "absolute",
                   bottom: 100,
                   zIndex: 1,
-                  alignSelf: 'center',
+                  alignSelf: "center",
                   height: 80,
                   width: 80,
                   flex: 1,
-                  justifyContent: 'center'
-                }}><Icon name='camera' style={{
-                  fontSize: 50,
-                }}/></Button>
-                <MyModal toggleModal={this.toggleModal} isModalVisible={this.state.isModalVisible}
-                         ocrText={this.state.ocrText} loading={this.state.loading}/>
-              </View>
-            </Camera>
-          </Container>
+                  justifyContent: "center"
+                }}
+              >
+                <Icon
+                  name="camera"
+                  style={{
+                    fontSize: 50
+                  }}
+                />
+              </Button>
+              <MyModal
+                toggleModal={this.toggleModal}
+                isModalVisible={this.state.isModalVisible}
+                ocrText={this.state.ocrText}
+                loading={this.state.loading}
+              />
+            </View>
+          </Camera>
+        </Container>
       );
     }
   }
--- src/components/indexScreen/MyModal.tsx
+++ src/components/indexScreen/MyModal.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import {View, Clipboard, ScrollView, TouchableOpacity} from "react-native";
+import React from "react";
+import { View, Clipboard, ScrollView } from "react-native";
 import Modal from "react-native-modal";
-import {Toast, Button, Card, CardItem, Spinner, Text, Icon} from "native-base";
+import {
+  Toast,
+  Button,
+  Card,
+  CardItem,
+  Spinner,
+  Text,
+  Icon
+} from "native-base";
 
-type Props = {
+interface Props {
   isModalVisible: boolean;
   toggleModal: () => void;
   ocrText: string;
@@ -14,18 +22,18 @@ const MyModal = (props: Props) => {
   const cardContent = () => {
     if (props.loading) {
       return (
-          <CardItem style={{justifyContent: 'center'}}>
-            <Spinner color='blue' />
-          </CardItem>
-      )
+        <CardItem style={{ justifyContent: "center" }}>
+          <Spinner color="blue" />
+        </CardItem>
+      );
     } else {
       return (
-          <CardItem>
-            <ScrollView style={{maxHeight: 300}}>
-              <Text selectable>{props.ocrText}</Text>
-            </ScrollView>
-          </CardItem>
-      )
+        <CardItem>
+          <ScrollView style={{ maxHeight: 300 }}>
+            <Text selectable>{props.ocrText}</Text>
+          </ScrollView>
+        </CardItem>
+      );
     }
   };
 
@@ -33,38 +41,41 @@ const MyModal = (props: Props) => {
     Clipboard.setString(props.ocrText);
     props.toggleModal();
     Toast.show({
-      text: 'Copy Success',
-      buttonText: 'close',
+      text: "Copy Success",
+      buttonText: "close",
       duration: 4000,
       type: "success",
       position: "bottom"
-    })
+    });
   };
 
   return (
-      <View style={{flex: 1}}>
-        <Modal isVisible={props.isModalVisible} >
-          <View style={{
+    <View style={{ flex: 1 }}>
+      <Modal isVisible={props.isModalVisible}>
+        <View
+          style={{
             flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
+            justifyContent: "center",
+            alignItems: "center"
           }}
-          >
-            <Card style={{zIndex: 1}}>
-              { cardContent() }
-              <CardItem style={{
-                flexDirection: 'column'
-              }}>
-                <Button onPress={copyText} block iconLeft>
-                  <Icon name='copy'/>
-                  <Text>copy text</Text>
-                </Button>
-              </CardItem>
-            </Card>
-          </View>
-        </Modal>
-      </View>
-  )
-}
+        >
+          <Card style={{ zIndex: 1 }}>
+            {cardContent()}
+            <CardItem
+              style={{
+                flexDirection: "column"
+              }}
+            >
+              <Button onPress={copyText} block iconLeft>
+                <Icon name="copy" />
+                <Text>copy text</Text>
+              </Button>
+            </CardItem>
+          </Card>
+        </View>
+      </Modal>
+    </View>
+  );
+};
 
-export default MyModal
+export default MyModal;
